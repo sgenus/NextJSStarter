e13efdd4443ca6ea27b38da3a1394afeb88e41e6
sgenus <samuel.genus@gmail.com>
2025-01-15 19:31:15 -0500

    Initial Version

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="p-2.5">
+      <h1 className="text-3xl font-bold underline text-center">Next JS Starter</h1>
+    </div>
+  );
+}
